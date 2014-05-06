8fd79633ced31d6b0e6d46037b06ed8238369207
adeng21 <adeng21@gmail.com>
2014-05-06 17:46:26 -0400

    changed job order

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @jobs = Job.all
+    @jobs = Job.order_by_newest
   end
 
   def new
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,6 @@
 class Job < ActiveRecord::Base
+
+  def self.order_by_newest
+    order(application_date: :desc)
+  end
 end
